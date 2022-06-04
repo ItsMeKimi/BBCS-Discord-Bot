7ad53c44882e4a21d2ee3dcad092903d4e3574cc
ItsMeKimi <yap.yuhung.kimi@dhs.sg>
2022-06-05 01:30:17 +0800

    Double support with 2 bots (for testing and for stable)

--- stable/app.js
+++ stable/app.js
@@ -1,3 +1,5 @@
+// Specific deployment for BBCS Discord.
+
 const { Client, Intents } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
@@ -17,7 +19,7 @@ client.on("ready", () => {
 	client.user.setActivity("out for / commands", { type: "WATCHING" });
 	console.log("Bot Connected To Discord!");
 
-	const channel = client.channels.cache.get("980466966626197537") // To Change
+	const channel = client.channels.cache.get("698868628459749407") // #verification Channel
 });
 
 // For Server Authentication
@@ -26,7 +28,7 @@ client.on("guildMemberAdd", async (member) => {
 
 	let guild = member.guild;
 	// let memberTag = member.user.tag;
-	const channel = client.channels.cache.get("980466966626197537") // To Change
+	const channel = client.channels.cache.get("698868628459749407") // #verification Channel
 
 	setTimeout(() => {
 		channel.send(
@@ -34,8 +36,8 @@ client.on("guildMemberAdd", async (member) => {
 			guild.name +
 			`**, <@${member.user.id}>! You are our ` +
 			guild.memberCount +
-			`th coder! Please use the command \`\/verify {School Initials} {Full Name} {Participant | Organiser}\` in this chat to verify your identity before proceeding. Do ensure that your School Initials + Full Name is under *32 characters*. An example would be \`\/confirm OJC Lim Ah Seng Participant\` for Lim Ah Seng, a participant from Original Junior College`
-			// `th coder! Please use the command \`\/verify {School Initials} {Full Name} {Participant | Organiser}\` in this chat to verify your identity before proceeding. Do ensure that your School Initials + Full Name is under *32 characters*. An example would be \`\/confirm OJC Lim Ah Seng Participant\` for Lim Ah Seng, a participant from Original Junior College **To sign up for workshops and events, kindly head over to https://go.buildingblocs.sg/signup to register for your tickets!**` // For during BBCs events
+			`th coder! Please use the command \`\/verify {Participant | Organiser} {School Initials} {Full Name}\` in this chat to verify your identity before proceeding. Do ensure that your School Initials + Full Name is under *32 characters*. An example would be \`\/verify Participant OJC Lim Ah Seng\` for Lim Ah Seng, a participant from Original Junior College`
+			// `th coder! Please use the command \`\/verify {Participant | Organiser} {School Initials} {Full Name}\` in this chat to verify your identity before proceeding. Do ensure that your School Initials + Full Name is under *32 characters*. An example would be \`\/verify Participant OJC Lim Ah Seng\` for Lim Ah Seng, a participant from Original Junior College **To sign up for workshops and events, kindly head over to https://go.buildingblocs.sg/signup to register for your tickets!**` // For during BBCs events
 		);
 	  }, 1000);
 }); 
@@ -51,11 +53,11 @@ const slashCommands = [
 		.setDescription('Tests the bot. The message author should get a reply that only they can see.'),
 	new SlashCommandBuilder()
 		.setName('verify')
-		.setDescription('Verify your account. Usage: \/verify {School Initials} {Full Name} {Participant | Organiser}')
+		.setDescription('Verify your account. Usage: \/verify {Participant | Organiser} {School Initials} {Full Name}')
 		.addSubcommand(subcommand =>
 			subcommand
 				.setName('participant')
-				.setDescription('Verify as participant.')
+				.setDescription('Verify as a participant.')
 				.addStringOption(option =>
 					option
 						.setName('info')
@@ -67,7 +69,7 @@ const slashCommands = [
 		.addSubcommand(subcommand =>
 			subcommand
 				.setName('organiser')
-				.setDescription('Verify as organiser.')
+				.setDescription('Verify as an organiser.')
 				.addStringOption(option =>
 					option
 						.setName('info')
@@ -81,7 +83,7 @@ const slashCommands = [
 const rest = new REST({ version: '9' }).setToken(botSecretToken);
 
 // Add them to guild
-rest.put(Routes.applicationGuildCommands(appID, guildID), { body: slashCommands})
+rest.put(Routes.applicationGuildCommands(appID, guildID), {body: slashCommands})
 	.then(() => console.log('Successfully registered slash commands.'))
 	.catch(console.error);
 
@@ -89,13 +91,15 @@ rest.put(Routes.applicationGuildCommands(appID, guildID), { body: slashCommands}
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+	const { commandName, channelId } = interaction;
+
+	if (channelId !== '698868628459749407') { // #verification Channel | BBCS Discord Specific
+		await interaction.reply({ content:`Hi <@${interaction.user.id}>! This bot currently only works for the verification channel.`, ephemeral: true });
+		return;
+	};
 
 	if (commandName === 'help') {
-		await interaction.reply({
-			content: ``,
-			ephemeral: true
-		});
+		await interaction.reply({ content: `Hi! Please follow the instructions in this Channel's description to proceed. If you are facing continuous errors, please contact <@375841238013706240>`, ephemeral: true });
 	} else if (commandName === 'test') {
 		await interaction.reply({ content: `Hi! This is a test message that only you can see.`, ephemeral: true});
 	} else if (commandName === 'verify') {
@@ -122,21 +126,21 @@ client.on('interactionCreate', async interaction => {
 			await interaction.reply({ content:`Error! Please ensure that you have written both your School Initials and your Full Name`, ephemeral: true });
 		} else {
 			if (schoolPortion.toUpperCase() === 'OJC') {
-				await interaction.reply({ content: `Hi <@${interaction.user.id}>! Please read the verification instructions carefully and not copy the example given. Feel free to message the OICs if you need any help.`, ephemeral: true });
+				await interaction.reply({ content: `Hi <@${interaction.user.id}>! Please read the verification instructions carefully and not copy the example given. Feel free to message the OICs or contact <@375841238013706240> if you need any help.`, ephemeral: true });
 			} else if (inputValue.length > 32) {
-				await interaction.reply({ content: `Hi <@${interaction.user.id}>! the command you entered was too long. Please ensure that you have entered the right information. If possible, use a short form of your name. If not possible, feel free to message the OICs for further help.`, ephemeral: true });
+				await interaction.reply({ content: `Hi <@${interaction.user.id}>! the command you entered was too long. Please ensure that you have entered the right information. If possible, use a short form of your name. If not possible, feel free to message the OICs or contact <@375841238013706240> for further help.`, ephemeral: true });
 			} else if (roleChoice.toUpperCase() === 'PARTICIPANT' | roleChoice.toUpperCase() === 'ORGANISER') {
 				await interaction.reply({ content: `Thank you <@${interaction.user.id}>! You can now access the other channels`, ephemeral: true });
 				setTimeout(() => {}, 3000)
 				// add 'verified' role
-				await interaction.member.roles.add('980840509654323222'); // To change
+				await interaction.member.roles.add('952074561262858280'); // BBCS Discord Specific
 				// add 'bbcs2022' role
-				await interaction.member.roles.add('980840703775092788'); // To change
+				await interaction.member.roles.add('952074439477051424'); // BBCS Discord (2022) Specific
 				
 				if (roleChoice.toUpperCase() === 'PARTICIPANT') {
-					await interaction.member.roles.add('980840801435279360');
+					await interaction.member.roles.add('698902585742196747'); // BBCS Discord Specific
 				} else if (roleChoice.toUpperCase() === 'ORGANISER') {
-					await interaction.member.roles.add('980840793705185340');
+					await interaction.member.roles.add('698841628324134923'); // BBCS Discord Specific
 				};
 
 				// In a try catch loop in case some member has higher roles than the bot
@@ -148,7 +152,7 @@ client.on('interactionCreate', async interaction => {
 				};
 				
 			} else {
-				await interaction.reply({ content: `Hi <@${interaction.user.id}>! There is an error somewhere in your input. Make sure everything you have typed is correct. If it still returns this error, feel free to message the OICs if you need any help.`, ephemeral: true });
+				await interaction.reply({ content: `Hi <@${interaction.user.id}>! There is an error somewhere in your input. Make sure everything you have typed is correct. If it still returns this error, feel free to message the OICs or contact <@375841238013706240> if you need any help.`, ephemeral: true });
 			};
 			
 		};
@@ -157,15 +161,17 @@ client.on('interactionCreate', async interaction => {
 
 });
 
-client.on('messageCreate', async (message) => {
-	messageContent = message.content
-	const channel = client.channels.cache.get("980466966626197537") // To Change
+client.on('messageCreate', (message) => {
+	let messageContent = message.content;
+	let messageChannelID = message.channelId;
 	
-	if (messageContent.startsWith('/verify')) {
-		message.send({ content: `Hi <@${interaction.user.id}>! Please.`, ephemeral: false })
-	}
-	console.log(messageContent)
-	console.log(message)
+	if (messageChannelID === '698868628459749407') {// #verification channel | BBCS Discord Specific
+		const channel = client.channels.cache.get(messageChannelID);
+
+		if (messageContent.startsWith('/verify')) {
+			channel.send(`Hi <@${message.author.id}>! Please use the correct format for \/verify to proceed.`);
+		};
+	};
 })
 
 client.login(botSecretToken);
